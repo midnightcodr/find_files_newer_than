6262563664d965a292b51fc39868a9ddb3687f44
Rico Chen <ricochen27@gmail.com>
2012-12-04 16:46:01 -0500

    added filtering feature

--- main.pl
+++ main.pl
@@ -3,15 +3,19 @@ use warnings;
 use strict;
 
 sub newer_than {
-	my($dir, $last_file)=@_;
-	opendir(my $DH, $dir) || die "Can't open $dir\n";
-	my @files=sort grep { $_ !~ /^\.\.?$/ && $_ gt $last_file} readdir($DH);
-	close($DH);
-	return @files;
+  my ($dir, $reg, $last_file)=@_;
+  opendir(my $DH, $dir) || do {
+    die "Can't open $dir\n";
+    return;
+  };
+  my @files=sort grep { ! /^\.\.?$/ && /^$reg$/ && $_ gt $last_file} readdir($DH);
+  close($DH);
+  return @files;
 }
 
 my $dir='./data';
+my $reg='file\d+\.xml$';
 print "Newer than '':\n";
-print join(':', newer_than($dir, ''))."\n";
+print join(':', newer_than($dir, $reg, ''))."\n";
 print "Newer than 'file20121202.xml':\n";
-print join(':', newer_than($dir, 'file20121202.xml'))."\n";
+print join(':', newer_than($dir, $reg, 'file20121202.xml'))."\n";
