4ee6db9edcafae33ccef5cdebcad63548a02c745
Rico Chen <ricochen27@gmail.com>
2012-12-04 02:34:01 -0500

    exclude . and .. useful when filename to compare to is empty

--- main.pl
+++ main.pl
@@ -5,10 +5,13 @@ use strict;
 sub newer_than {
 	my($dir, $last_file)=@_;
 	opendir(my $DH, $dir) || die "Can't open $dir\n";
-	my @files=sort grep {$_ gt $last_file} readdir($DH);
+	my @files=sort grep { $_ !~ /^\.\.?$/ && $_ gt $last_file} readdir($DH);
 	close($DH);
 	return @files;
 }
 
 my $dir='./data';
-print join(':', newer_than($dir, 'file20121202.xml'));
+print "Newer than '':\n";
+print join(':', newer_than($dir, ''))."\n";
+print "Newer than 'file20121202.xml':\n";
+print join(':', newer_than($dir, 'file20121202.xml'))."\n";
